1a363e93a5ac6104db4b61c68f4c2528c0963aeb
Lucifer Uchiha <rowla101@gmail.com>
2021-03-25 11:13:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+KEYWORD = '$bt'
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author != client.user and message.content.startswith(KEYWORD):
+        await message.channel.send('Hello, I am your bartender')
+
+
+
+client.run(TOKEN)
