c884362b9ba32f653be7f6bf303091ed1d48b54c
Lucifer Uchiha <rowla101@gmail.com>
2021-03-25 16:06:02 +0100

    Implemented recipe for cocktails command

--- bartender.py
+++ bartender.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Bartender:
     with open("recipes.json") as recipes_file:
         recipes = json.load(recipes_file)
@@ -23,18 +24,21 @@ class Bartender:
         elif argument.startswith("count"):
             parameters = self.remove_from_word(argument, "count").strip()
             answer = self.get_count(parameters)
+        elif argument.startswith("recipe"):
+            parameters = self.remove_from_word(argument, "recipe").strip()
+            answer = self.get_recipe(parameters)
         return answer
 
     def get_all_ingredients(self):
         answer = ""
         for key, value in self.ingredients.items():
-            answer += key + " (" + str(value.get("abv")) + "%)\n"
+            answer += f"{key} ({value.get('abv')}%)\n"
         return answer
 
     def get_all_cocktails(self):
         answer = ""
         for cocktail in self.recipes:
-            answer += cocktail.get("name") + "\n"
+            answer += f"{cocktail.get('name')}\n"
         return answer
 
     def remove_from_word(self, word, toRemove):
@@ -42,12 +46,48 @@ class Bartender:
 
     def get_count(self, parameters):
         print(parameters)
-        answer = ""
+        answer = self.error
         if parameters == "ingredients":
             answer = len(self.ingredients)
         elif parameters == "cocktails":
             answer = len(self.recipes)
-        else:
-            answer = self.error
+
         return answer
 
+    def get_recipe(self, parameter):
+        answer = f"There is no recipe for a cocktail called {parameter}. To see all cocktails with a recipe " \
+                 f"type '$bt cocktails'"
+        for cocktail in self.recipes:
+            formatted_ingredients = self.get_formatted_ingredients(cocktail.get("ingredients"))
+            garnish = self.get_garnisch(cocktail)
+            if parameter == cocktail.get("name").lower():
+                return f"__**{cocktail.get('name')}**__\n" \
+                       f"**Ingriedients:**\n" \
+                       f"{formatted_ingredients}" \
+                       f"{garnish}" \
+                       f"**Preparation:**\n" \
+                       f"{cocktail.get('preparation')} \n" \
+
+
+        return answer
+
+    def get_formatted_ingredients(self, ingredients):
+        formatted_ingredients = ""
+        special_ingredients = ""
+        for ingredient in ingredients:
+            if ingredient.get("special") is not None:
+                special_ingredients += f" - {ingredient.get('special')}\n"
+            else:
+                formatted_ingredients += f" - {ingredient.get('amount')} {ingredient.get('unit')} {ingredient.get('ingredient')} "
+                if ingredient.get("label") is not None:
+                    f"({ingredient.get('label')})"
+                formatted_ingredients += "\n"
+
+        return formatted_ingredients + special_ingredients
+
+    def get_garnisch(self, cocktail):
+        if cocktail.get("garnish") is not None:
+            return f"**Garnish:**\n" \
+                   f" - {cocktail.get('garnish')} \n"
+        else:
+            return ""
