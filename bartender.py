1f6e769664c47291a948add227f99969eff5343b
Lucifer Uchiha <rowla101@gmail.com>
2021-03-25 14:25:52 +0100

    implemented list inrgedients command

---
+++ bartender.py
@@ -0,0 +1,30 @@
+import json
+
+
+class Bartender:
+    with open("recipes.json") as recipes_file:
+        recipes = json.load(recipes_file)
+
+    with open("ingredients.json") as ingredients_file:
+        ingredients = json.load(ingredients_file)
+
+    def handle(self, message):
+        switcher = {
+            "help": self.help,
+            "ingredients": self.list_all_ingredients
+        }
+        print(message)
+        argument = message.content[3:].strip()
+        func = switcher.get(argument, "This command does not exist, to see which commands exist type '$bt help'")
+        # Execute the function
+        return func()
+
+    def help(self):
+        # TODO write a list of all commands
+        return "This will list all commands"
+
+    def list_all_ingredients(self):
+        string = ""
+        for key, value in self.ingredients.items():
+            string += key + " (" + str(value.get("abv")) + "%)\n"
+        return string
--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import os
-import json
 import discord
 from dotenv import load_dotenv
 
+from bartender import Bartender
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-KEYWORD = '$bt'
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+KEYWORD = "$bt"
 
 client = discord.Client()
-
-with open('recipes.json') as recipes_file:
-    recipes = json.load(recipes_file)
-
-with open('ingredients.json') as ingredients_file:
-    ingredients = json.load(ingredients_file)
 
 @client.event
 async def on_ready():
@@ -23,14 +18,14 @@ async def on_ready():
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f"{client.user} is connected to the following guild:\n"
+        f"{guild.name}(id: {guild.id})"
     )
 
 @client.event
 async def on_message(message):
     if message.author != client.user and message.content.startswith(KEYWORD):
-        await message.channel.send('Hello, I am your bartender')
+        await message.channel.send(Bartender().handle(message))
 
 
 
